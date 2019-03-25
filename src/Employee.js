5db7bb269cbdfb667d4b65f155917543e6dbe05b
rodrigotorrico <rodrigo.torricodelcastillo@hotmail.com>
2019-03-25 17:19:00 -0400

    Implemented basic test for Salary Calculator

---
+++ src/Employee.js
@@ -0,0 +1,6 @@
+export  default class Employee {
+    constructor(name, type){
+        this.name = name;
+        this.type = type;
+    }
+}
---
+++ src/Util/SalaryCalculator.js
@@ -0,0 +1,9 @@
+export default  class SalaryCalculator{
+    static calculate(employee){
+        switch(employee.type){
+            case 'fixed':
+                return 100;
+        }
+    }
+
+}
