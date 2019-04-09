03c7a0f0950fd46d12d7592b50f61efa577b1b27
whiletruedothis <rodrigo.torricodelcastillo@gmail.com>
2019-04-09 12:23:51 -0400

    added some comments

--- test/Boleta.test.js
+++ test/Boleta.test.js
@@ -8,14 +8,14 @@ import RegistroDeVenta from '../src/Hojas/RegistroDeVenta.js';
 describe('Funciones basicas de la calculadora de salario', () => {
 
 
-    it('Generar boleta de salario para Carlos que es empleado fijo con 2000$ de salario', () => {
+    it('Deberia generar boleta de salario para Carlos que es empleado fijo con 2000$ de salario', () => {
         let Carlos = new Empleado('Carlos');
         Carlos.esEmpleadoFijo(2000);
         let BoletaDeCarlos = new Boleta(Carlos);
         expect(BoletaDeCarlos.salario).equal(2000);
     })
 
-    it('Generar boleta de salario para Roxana que es un empleado por hora', () => {
+    it('Deberia generar boleta de salario para Roxana que es un empleado por hora', () => {
         let RegistroDeTiempoDeRoxana = new RegistroDeTiempo(7, 180);
         let Roxana = new Empleado('Roxana');
         Roxana.esEmpleadoPorHora(RegistroDeTiempoDeRoxana);
@@ -23,7 +23,7 @@ describe('Funciones basicas de la calculadora de salario', () => {
         expect(BoletaDeRoxana.salario).equal(1260);
     })
 
-    it('Generar boleta de salario para Juan que es un empleado por comision', () => {
+    it('Deberia generar boleta de salario para Juan que es un empleado por comision', () => {
         let RegistroDeVentaDeJuan = new RegistroDeVenta(2, 200);
         let Juan = new Empleado('Juan');
         Juan.esEmpleadoPorComision(RegistroDeVentaDeJuan,2000);
@@ -31,7 +31,7 @@ describe('Funciones basicas de la calculadora de salario', () => {
         expect(BoletaDeJuan.salario).equal(2400);
     })
 
-    it('Generar boleta de salario para Carlos que era un empleado fijo y ahora es por hora', () => {
+    it('Deberia generar boleta de salario para Carlos que era un empleado fijo y ahora es por hora', () => {
         let Carlos = new Empleado('Carlos');
         Carlos.esEmpleadoFijo(2000);
         let RegistroDeTiempoDeCarlos = new RegistroDeTiempo(7, 180);
@@ -40,7 +40,7 @@ describe('Funciones basicas de la calculadora de salario', () => {
         expect(BoletaDeCarlos.salario).equal(1260);
     })
 
-    it('Generar boleta de salario para Carlos que era un empleado fijo y ahora es por comision', () => {
+    it('Deberia generar boleta de salario para Carlos que era un empleado fijo y ahora es por comision', () => {
         let Carlos = new Empleado('Carlos');
         Carlos.esEmpleadoFijo(2000);
         let RegistroDeVentasDeCarlos = new RegistroDeVenta(2, 200);
@@ -48,5 +48,6 @@ describe('Funciones basicas de la calculadora de salario', () => {
         let BoletaDeCarlos = new Boleta(Carlos);
         expect(BoletaDeCarlos.salario).equal(2400);
     })
+
 
 })
