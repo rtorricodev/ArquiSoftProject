5e0653b368e984be59dac9fc17c61de5b26dca6b
whiletruedothis <rodrigo.torricodelcastillo@gmail.com>
2019-04-15 12:12:29 -0400

    se agrego los todos del proyecto al archivo readme

--- test/Boleta.test.js
+++ test/Boleta.test.js
@@ -9,22 +9,6 @@ describe('Funciones basicas de la calculadora de salario', () => {
 
     
 
-    //TODO: Si un empleado fijo empieza a medio mes entonces solo se le paga ese tiempo✅ //chino
-    //TODO: Si trabajo más de 8 horas se le paga se le paga más 1.5 más ✅ //chino
-    //TODO: En un futuro debe tener persistencia✅ // Rodri
-    //TOOD: mover calculadora de salario a empleado✅ //Chino
-    //TODO: Fecha en que corresponde pagar ✅
-    //TODO: Organizar tests✅ // Rodri
-    //TODO: Crear seteador de fecha✅ // Oscar
-    //TODO: Empleados eligen su metodo de pago : Deposito, cheque, efectivo RRHH ✅ //Oscar
-    //TODO: Empleados pueden elegir como reciben las notificaciones de su pago una o más whatsaap,email,sms, push notificaciones ✅ // oscar  y rodri
-    
-    //TODO: Se le envia boleto por email✅ // chino
-    //TODO: Empleados deben escoger la manera de su notificacion de pago whatsapp,email,SMS,Facebook
-    //TODO: Interfaz de usario debe ser web 
-    //TODO: Algunos empleados del sindicato pagan una cuota mensual Si tiene una deuda se le quita de su salario
-
-
     it('Deberia generar boleta de salario para Carlos que es empleado fijo con 2000$ de salario', () => {
         let Carlos = new Empleado('Carlos');
         Carlos.esEmpleadoFijo(2000);
