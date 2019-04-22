2f9b9e07db27f192c08872f5d8be340241f54d52
Nicolas Ponce de Leon <nicolaspdle@gmail.com>
2019-04-22 17:57:14 -0400

    Refactorizacion: CalculadoraFija

--- src/Clases/CalculadoraDeSalario/CalculadoraFija.js
+++ src/Clases/CalculadoraDeSalario/CalculadoraFija.js
@@ -1,10 +1,17 @@
 export default class CalculadoraFija{
     calcularMonto(empleado){
-        if(!empleado.recibioSuPrimerPago){
-            /*salario prorrateado*/
+        if(this.noRecibioSuPrimerPago(empleado)){
             empleado.recibioSuPrimerPago = true;
-            return (empleado.salarioFijo * (empleado.fechaInicioDeTrabajo.obtenerUltimoDiaMes() - empleado.fechaInicioDeTrabajo.dia + 1 )) / empleado.fechaInicioDeTrabajo.obtenerUltimoDiaMes();
+            return this.obtenerSalarioProrrateado(empleado);
         }
         return empleado.salarioFijo;
+    }
+
+    noRecibioSuPrimerPago(empleado){
+        return empleado.recibioSuPrimerPago === false;
+    }
+
+    obtenerSalarioProrrateado(empleado){
+        return (empleado.salarioFijo * (empleado.fechaInicioDeTrabajo.obtenerUltimoDiaMes() - empleado.fechaInicioDeTrabajo.dia + 1 )) / empleado.fechaInicioDeTrabajo.obtenerUltimoDiaMes();
     }
 }
