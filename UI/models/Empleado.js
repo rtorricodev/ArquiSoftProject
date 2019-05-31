bf3b38c8d6c88e5a69186828463bcb907dbfeaa5
Nicolas Ponce de Leon <nicolaspdle@gmail.com>
2019-05-31 13:58:20 -0400

    se orderno el frontend

---
+++ UI/models/Empleado.js
@@ -0,0 +1,10 @@
+class Empleado{
+    constructor(nombre, apellido, celularPrincipal, correoPrincipal, tipo, metodoPago) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.celularPrincipal = celularPrincipal;
+        this.correoPrincipal = correoPrincipal;
+        this.tipo = tipo;
+        this.metodoPago = metodoPago;
+    }
+};
